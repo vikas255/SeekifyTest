2846d20582e8ede023dd34831637d1b31fa27d99
unknown <iit2016058@iiita.ac.in>
2020-01-20 17:46:11 +0530

    for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int i=0;
-       whie(i<10)
+       for(int i=0;i<10;i++)
        {
            system.out.print(i);
            i++;
